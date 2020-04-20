0c04304e6f3de0ebe2282c76dc7df88540f62692
BuildTools <unconfigured@null.spigotmc.org>
2020-04-20 11:16:11 +0200

    .idea folder aan gitignore toegevoegd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Covid App
                 </div>
 
                 <div class="links">
