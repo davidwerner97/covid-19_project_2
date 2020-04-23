59194e85eab6e8d67aa0441d8ea1b5dfe6780b5c
BuildTools <unconfigured@null.spigotmc.org>
2020-04-23 10:35:24 +0200

    comment in template bestand

--- resources/views/temptemplate.blade.php
+++ resources/views/temptemplate.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
+{{--deze template is tijdelijk deze kan ik gebruiken om van te extenden om later de goede template gemakkelijk te kunnen gebruiken--}}
 <head>
     @yield('head')
         <link href="https://unpkg.com/material-components-web@v4.0.0/dist/material-components-web.min.css" rel="stylesheet">
