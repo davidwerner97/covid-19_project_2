49a1dc4be65b4d8c8c33c8cef5c8e3bb01eac948
BuildTools <unconfigured@null.spigotmc.org>
2020-04-23 10:34:13 +0200

    meerdere edits
    -namen van controller aangepast
    - db entries van string naar text
    - begonnen aan view

---
+++ app/Http/Controllers/WerknemersController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Http\Request;
+use App\Werknemer;
+
+class WerknemersController extends Controller
+{
+    public function index(){
+        //geef een lijst van werknemers
+        $werknemers = Werknemer::latest()->get();
+        return view('werknemer.index',["werknemers" => $werknemers]);
+    }
+
+    public function show($id){
+        //laat informatie van 1 werknemer zien
+        $werknemer = Werknemer::find($id);
+        return view('werknemers.show',["werknemer" => $werknemer]);
+    }
+
+    public function create(){
+        //laat view zien om een werknemer aan te maken
+        return view('werknemer.create');
+    }
+    public function store(){
+        //sla hem op
+        Werknemer::create($this->validateWerknemer());
+        return redirect(route('werknemer.store'));
+    }
+    public function edit(Werknemer $werknemer){
+        //laat view zien om werknemer te bewerken
+        return view('werknemer.edit', ['werknemer' =>$werknemer]);
+    }
+    public function update(Werknemer $werknemer){
+        //update de werknemer
+        $werknemer->update($this->validateWerknemer());
+        return redirect($werknemer->path());
+
+    }
+    public function destroy(Werknemer $werknemer){
+        //verwijder  en werknemer
+        $werknemer->delete();
+        $werknemers = Werknemer::latest()->get();
+        return view('werknemer.index', ['werknemers' => $werknemers]);
+    }
+
+    public function validateWerknemer() : array
+    {
+        return request()->validate([
+            'vnaam' => ['required'],
+            'anaam' => ['required'],
+            'plaats' => ['required'],
+            'postcode' => ['required'],
+            'hnummer' => ['required'],
+            'straat' => ['required'],
+            'telnummer' => ['required'],
+            'functie' => ['required'],
+            'bedrijf_id' => ['required'], //we moeten hier meer validatie doen om te kijken of het bedrijf bestaat en mishcen ook dat de bedrijf die het aanmaakt ook het bedrijf van de werknemer is
+        ]);
+
+    }
+
+
+    //
+}
---
+++ resources/views/temptemplate.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @yield('head')
+        <link href="https://unpkg.com/material-components-web@v4.0.0/dist/material-components-web.min.css" rel="stylesheet">
+        <script src="https://unpkg.com/material-components-web@v4.0.0/dist/material-components-web.min.js"></script>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body id="page-top">
+<div class="container-fluid">
+    @yield("inhoud")
+</div>
+</body>
+</html>
