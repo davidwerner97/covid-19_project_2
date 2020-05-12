b2672afc3548c205b0fe49a110fc8d639c228a10
David Werner <43265476+Codewarrior1997@users.noreply.github.com>
2020-05-12 09:18:06 +0200

    Update index view

--- resources/views/bedrijven/index.blade.php
+++ resources/views/bedrijven/index.blade.php
@@ -6,8 +6,9 @@
             display: flex;
             flex-wrap: wrap;
         }
-        .mdc-card{
-            float:left;
+
+        .mdc-card {
+            float: left;
         }
 
         .element-card {
@@ -18,12 +19,14 @@
         .element-card > .mdc-card__media {
             height: 9em;
         }
-        .mdc-card__media{
+
+        .mdc-card__media {
             text-align: left;
             margin: 10px;
 
         }
-        .mdc-card__media-content{
+
+        .mdc-card__media-content {
             display: inline-block;
         }
     </style>
@@ -31,13 +34,22 @@
 
 @section('inhoud')
     <div id="content">
-        <div class="mdc-card element-card earth">
-            <div class="mdc-card__media">
-                <div class="mdc-card__media-content">
-                    <div class="mdc-typography--headline4"></div>
-                    <a href=""><i aria-hidden="true" class="material-icons mdc-button__icon"><!---->delete<!----></i></a> <a href=""><i aria-hidden="true" class="material-icons mdc-button__icon"><!---->edit<!----></i></a>
+        @foreach($bedrijf as $bedrijf)
+            <div class="mdc-card element-card earth">
+                <div class="mdc-card__media">
+                    <div class="mdc-card__media-content">
+                        <div class="mdc-typography--headline4">{{$bedrijf->naam}}</div>
+                        <div class="mdc-typography--headline9">{{ $bedrijf->plaats}}</div>
+                        <div class="mdc-typography--headline9">{{$bedrijf->postcode}}</div>
+                        <div class="mdc-typography--headline9">{{$bedrijf->straat. ' ' . $bedrijf->hnummer}}</div>
+                        <div class="mdc-typography--headline9">{{$bedrijf->telnummer}}</div>
+                        <a href="{{$bedrijf->path()}}}"><i aria-hidden="true" class="material-icons mdc-button__icon"><!---->delete
+                                <!----></i></a> <a href=""><i aria-hidden="true"
+                                                              class="material-icons mdc-button__icon">
+                                <!---->edit<!----></i></a>
+                    </div>
                 </div>
             </div>
-        </div>
+        @endforeach
     </div>
 @endsection
